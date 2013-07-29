bd8690f2ff2b91f163884986934ee87ffef3b86f
David Gibbons <dag@rentrak.com>
2013-07-29 11:48:46 -0700

    changed blue to be lighter

--- rpi_twitter.py
+++ rpi_twitter.py
@@ -191,7 +191,7 @@ class pyscope :
         white = (255, 255, 255) 
         red = (255, 0, 0)
         green = (0, 255, 0)
-        blue = (0, 0, 255)
+        blue = (0, 100, 200)
         black = (0, 0, 0)
         
         font_size = 45
